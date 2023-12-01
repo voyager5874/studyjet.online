160bff23008445e0015e84c44dd9aae0b142b640
Aleksandr Savkin <voyager5874@gmail.com>
2023-12-01 17:28:27 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
+
+
+module.exports = {
+  extends: '@it-incubator/eslint-config',
+  overrides: [
+    {
+      files: ['**/*.stories.tsx'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/consistent-type-imports': ['error', {
+          prefer: 'type-imports'
+        }]
+      },
+    }
+  ],
+}
