966d9a5e61de706558de6751aa83089aad50986b
Aleksandr Savkin <voyager5874@gmail.com>
2023-12-08 18:04:39 +0300

    feat: create TextField component

---
+++ src/ui/text-field/text-field.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { useArgs } from '@storybook/preview-api'
+
+import { TextField } from './text-field'
+
+const meta = {
+  title: 'App/TextField',
+  component: TextField,
+  tags: ['autodocs'],
+  argTypes: {
+    value: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    type: {
+      control: 'radio',
+      options: ['password', 'search', 'text'],
+    },
+  },
+} satisfies Meta<typeof TextField>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const Template: Story = {
+  render: args => {
+    const { value, ...restArgs } = args
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [, setArgs] = useArgs()
+
+    const updateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setArgs({ ...args, value: e.target.value })
+    }
+
+    const handleClear = () => {
+      setArgs({ ...args, value: '' })
+    }
+
+    return <TextField {...restArgs} onChange={updateValue} onClear={handleClear} value={value} />
+  },
+}
+
+export const Overview: Story = {
+  ...Template,
+  args: {
+    label: 'Label',
+    placeholder: 'placeholder',
+    type: 'text',
+  },
+}
---
+++ src/ui/text-field/text-field.tsx
@@ -0,0 +1,76 @@
+import type { ComponentPropsWithoutRef, ForwardedRef } from 'react'
+import { forwardRef, useState } from 'react'
+
+import { Button } from '@/ui/button'
+import { TextFieldBase } from '@/ui/text-field/text-field-base'
+import { Eye, EyeOff, Search, X } from 'lucide-react'
+
+export type Props = { onClear?: () => void } & ComponentPropsWithoutRef<typeof TextFieldBase>
+
+const RenderFunction = (props: Props) => {
+  const { disabled, type, value, onClear, ...restProps } = props
+  const [showContent, setShowContent] = useState(false)
+
+  const getPrefix = () => {
+    if (type === 'search') {
+      return <Search size={14} />
+    }
+
+    return null
+  }
+
+  const handleClear = () => {
+    onClear && onClear()
+  }
+
+  const getSuffix = () => {
+    if (type === 'password') {
+      return showContent ? (
+        <Button disabled={disabled} onClick={() => setShowContent(prev => !prev)} variant={'icon'}>
+          <Eye size={14} />
+        </Button>
+      ) : (
+        <Button disabled={disabled} onClick={() => setShowContent(prev => !prev)} variant={'icon'}>
+          <EyeOff size={14} />
+        </Button>
+      )
+    }
+    if (type === 'search' && value) {
+      return (
+        <Button disabled={disabled} onClick={handleClear} variant={'icon'}>
+          <X size={14} />
+        </Button>
+      )
+    }
+
+    return null
+  }
+
+  return (
+    <TextFieldBase
+      {...restProps}
+      disabled={disabled}
+      prefixIcon={getPrefix()}
+      suffixIcon={getSuffix()}
+      type={getInputType(type, showContent)}
+      value={value}
+    />
+  )
+}
+
+const TextField = forwardRef(RenderFunction) as (
+  props: Props & { ref?: ForwardedRef<typeof TextFieldBase> }
+) => ReturnType<typeof RenderFunction>
+
+function getInputType<T>(initialType: T, showContent: boolean): T {
+  if (initialType === 'password' && !showContent) {
+    return 'password' as T
+  }
+  if (initialType === 'password' && showContent) {
+    return 'text' as T
+  }
+
+  return initialType
+}
+
+export { TextField }
