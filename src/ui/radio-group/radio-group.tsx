3a45c1dd0d7ffa523e8058196d2e3892eeca291c
Aleksandr Savkin <voyager5874@gmail.com>
2023-12-14 17:23:55 +0300

    fix: ref was not passed to the underlying elements

--- src/ui/radio-group/radio-group.tsx
+++ src/ui/radio-group/radio-group.tsx
@@ -1,23 +1,25 @@
-import type { ComponentPropsWithoutRef, ForwardedRef } from 'react'
+import type { ComponentPropsWithoutRef, ElementRef } from 'react'
 import { forwardRef } from 'react'
+
+import { Button } from '@/ui/button'
 
 import { RadioGroupItem, RadioGroupRoot } from './radio-group-base'
 
 type Props = { items: { key: string; value: string }[] } & ComponentPropsWithoutRef<
   typeof RadioGroupRoot
 >
-const RenderFunction = (props: Props) => {
+const RadioGroup = forwardRef<ElementRef<typeof RadioGroupRoot>, Props>((props, ref) => {
   const { disabled, items, ...rest } = props
 
   return (
-    <RadioGroupRoot {...rest} disabled={disabled}>
+    <RadioGroupRoot {...rest} disabled={disabled} ref={ref}>
       {items.map(item => (
         <RadioGroupItem disabled={disabled} id={item.key} key={item.key} value={item.value} />
       ))}
     </RadioGroupRoot>
   )
-}
+})
 
-export const RadioGroup = forwardRef(RenderFunction) as (
-  props: Props & { ref?: ForwardedRef<typeof RadioGroupRoot> }
-) => ReturnType<typeof RenderFunction>
+Button.displayName = 'RadioGroup'
+
+export { RadioGroup }
