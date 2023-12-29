a8deb99a2a16f3cc8e655e4924a259ca6ea80bee
Aleksandr Savkin <voyager5874@gmail.com>
2023-12-29 13:49:49 +0300

    refactor: process images errors (wrong file format uploaded)

--- src/features/decks/table/deck-cover.tsx
+++ src/features/decks/table/deck-cover.tsx
@@ -1,5 +1,6 @@
 import type { DeckItem } from '@/features/decks'
 
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Typography } from '@/ui/typography'
@@ -8,14 +9,23 @@ import { clsx } from 'clsx'
 import s from './deck-cover.module.scss'
 
 export const DeckCover = ({ deck }: { deck: DeckItem }) => {
+  const [src, setSrc] = useState<null | string>(deck.cover)
+
+  const handleError = () => {
+    setSrc(null)
+  }
+
   const classNames = {
     container: clsx(s.container),
     image: clsx(s.image),
   }
+  // todo: use some placeholder if there is no cover / maybe two different: for error and actual null
 
   return (
     <div className={classNames.container} style={{ display: 'flex', alignItems: 'center' }}>
-      {deck.cover && <img alt={'deck cover'} className={classNames.image} src={deck.cover} />}
+      {src && (
+        <img alt={'deck cover'} className={classNames.image} onError={handleError} src={src} />
+      )}
       <Typography as={Link} to={`/decks/${deck.id}/cards`} variant={'link1'}>
         {deck.name}
       </Typography>
