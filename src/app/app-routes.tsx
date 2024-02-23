09ad970558374852c4d46d63227eb34583f7c434
Aleksandr Savkin <voyager5874@gmail.com>
2024-02-23 14:10:44 +0300

    refactor: add 'key' property for dynamic creation of const for keeping all app routes as index signature object

--- src/app/app-routes.tsx
+++ src/app/app-routes.tsx
@@ -1,4 +1,5 @@
 import { CardsOfDeckPage } from '@/pages/cards-of-deck'
+import { CheckEmailPromptPage } from '@/pages/check-email-prompt'
 import { DecksPage } from '@/pages/decks'
 import { LearnPage } from '@/pages/learn'
 import { RequestPasswordResetPage } from '@/pages/password-reset-request'
@@ -9,23 +10,59 @@ import { UserPage } from '@/pages/user'
 
 export const AppRoutes = {
   private: [
-    { element: <DecksPage />, name: 'Decks', path: '/decks' },
-    { element: <UserPage />, name: 'User profile', path: '/user' },
-    { element: <CardsOfDeckPage />, name: 'Cards of a deck', path: 'decks/:id/cards' },
-    { path: 'decks/:id/learn', element: <LearnPage />, name: 'Learn a deck' },
+    { element: <DecksPage />, name: 'Decks', path: '/decks', key: 'decks' },
+    { element: <UserPage />, name: 'User profile', path: '/user', key: 'userProfile' },
+    {
+      element: <CardsOfDeckPage />,
+      name: 'Cards of a deck',
+      path: 'decks/:id/cards',
+      key: 'cardsOfDeck',
+    },
+    { element: <LearnPage />, name: 'Learn a deck', path: 'decks/:id/learn', key: 'learnDeck' },
   ],
   public: [
     {
       element: <RequestPasswordResetPage />,
       name: 'Request password reset',
-      path: '/password-reset',
+      path: '/password-reset-request',
+      key: 'requestPasswordReset',
     },
-    { element: <SetNewPasswordPage />, name: 'Set new password', path: '/password-set' },
-    { element: <SignInPage />, name: 'Sign in', path: '/sign-in' },
-    { element: <SignUpPage />, name: 'Sign up', path: '/sign-up' },
+    {
+      element: <SetNewPasswordPage />,
+      name: 'Set new password',
+      path: '/password-set-new/:token',
+      key: 'setNewPassword',
+    },
+    { element: <SignInPage />, name: 'Sign in', path: '/sign-in', key: 'signIn' },
+    { element: <SignUpPage />, name: 'Sign up', path: '/sign-up', key: 'signUp' },
+    {
+      element: <CheckEmailPromptPage />,
+      name: 'Check email',
+      path: '/check-email/:email',
+      key: 'checkEmail',
+    },
   ],
 } as const
 
 export type AppRoutesPath =
   | (typeof AppRoutes)['private'][number]['path']
   | (typeof AppRoutes)['public'][number]['path']
+
+type AppRoutesKey =
+  | (typeof AppRoutes)['private'][number]['key']
+  | (typeof AppRoutes)['public'][number]['key']
+
+type PathsByKey = {
+  [key in AppRoutesKey]: AppRoutesPath
+}
+
+export const paths: PathsByKey = {
+  ...AppRoutes.private.reduce((acc, route) => {
+    return { ...acc, [route.key]: route.path }
+  }, {} as PathsByKey),
+  ...AppRoutes.public.reduce((acc, route) => {
+    return { ...acc, [route.key]: route.path }
+  }, {} as PathsByKey),
+}
+
+console.log({ paths })
