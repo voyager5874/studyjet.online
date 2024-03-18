9f7eaa96a4796575429e012fe999c8fd600b3ac9
Aleksandr Savkin <voyager5874@gmail.com>
2024-03-18 10:36:46 +0300

    refactor: use storybook context

--- src/storybook-utils/current-theme-provider.tsx
+++ src/storybook-utils/current-theme-provider.tsx
@@ -1,21 +1,28 @@
 import type { ComponentPropsWithoutRef } from 'react'
-import { createContext } from 'react'
+import { useEffect } from 'react'
 
 import s from './custom-css-provider.module.scss'
 
-export const StorybookThemeContext = createContext<'dark' | 'light'>('dark')
+// export const StorybookThemeContext = createContext<'dark' | 'light'>('dark')
+type Props = {
+  backgrounds?: { name: string; value: string }[]
+  currentBackground?: string
+} & ComponentPropsWithoutRef<'div'>
+export const CurrentThemeProvider = ({ currentBackground, backgrounds, ...rest }: Props) => {
+  const darkBackground = backgrounds ? backgrounds.find(option => option.name === 'dark') : null
 
-export const CurrentThemeProvider = ({ ...rest }: ComponentPropsWithoutRef<'div'>) => {
-  const params = new URLSearchParams(document.location.search)
-  const storybookTheme = params.get('globals')?.includes('backgrounds.value:!hex(f9f7ff)')
-    ? 'light'
-    : 'dark'
+  useEffect(() => {
+    if (currentBackground && darkBackground) {
+      const theme = currentBackground === darkBackground.value ? 'dark' : 'light'
 
-  document.body.dataset.theme = storybookTheme
+      document.body.setAttribute('data-theme', theme)
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentBackground])
 
   return (
-    <StorybookThemeContext.Provider value={storybookTheme}>
-      <div {...rest} className={s.root} />
-    </StorybookThemeContext.Provider>
+    // <StorybookThemeContext.Provider value={storybookTheme}>
+    <div {...rest} className={s.root} />
+    // </StorybookThemeContext.Provider>
   )
 }
